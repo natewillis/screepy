433e0c4ce300bd798dcb34d349f87e5d569e6ce1
Nate Willis <natewillis1@gmail.com>
2020-10-30 14:42:35 -0500

    logging

--- src/prototypes/spawn.js
+++ src/prototypes/spawn.js
@@ -10,8 +10,10 @@ Spawn.prototype.execute_task = function (task) {
 
     // spawn action
     if (task.type == 'spawnCreep') {
+        console.log('spawn is producing creep on tick ' + task.tick);
         task.executed_return_value = spawn.spawnCreep(task.details.body, task.details.name);
     } else {
+        console.log('spawn is executing unknown task');
         task.executed_return_value = 99;
     }
 
